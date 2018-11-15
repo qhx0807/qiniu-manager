7b7e40a1282823c79202c9c3fa2512c1dfd4386b
qhx0807 <820240134@qq.com>
2018-11-15 23:05:36 +0800

    add document

--- controllers/bucketController.js
+++ controllers/bucketController.js
@@ -13,7 +13,12 @@ const Host_Config = {
 const mac = new qiniu.auth.digest.Mac(qiniuConfig.ACCESS_KEY, qiniuConfig.SECRET_KEY)
 
 class BucketController {
-
+  /**
+   * 获取 Bucket 列表
+   * @method GET
+   * @param {}
+   * @returns {Array}
+   */
   static async buckets (ctx) {
     let requestURI = Host_Config.host_1 + '/buckets'
     let AccessToken = qiniu.util.generateAccessToken(mac, requestURI)
@@ -24,7 +29,11 @@ class BucketController {
       return ctx.error({data: error.toString()})
     }
   }
-
+  /**
+   * 创建 Bucket
+   * @method POST
+   * @param {name, regin}
+   */
   static async mkbucket (ctx) {
     let name = ctx.request.body.name
     let region = ctx.request.body.region
@@ -38,7 +47,11 @@ class BucketController {
       return ctx.error({data: error.toString()})
     }
   }
-
+  /**
+   * 删除 Bucket
+   * @method GET
+   * @param {bucket}
+   */
   static async dropbuctet (ctx) {
     let bucketName = ctx.params.bucket
     let requestURI = `${Host_Config.host_2}/drop/${bucketName}`
@@ -50,7 +63,12 @@ class BucketController {
       return ctx.error({data: error.toString()})
     }
   }
-
+  /**
+   * 获取 Bucket 空间域名
+   * @method GET
+   * @param {bucket}
+   * @returns {Array}
+   */
   static async domainlist (ctx) {
     let bucketName = ctx.params.bucket
     let requestURI = `${Host_Config.host_3}/v6/domain/list?tbl=${bucketName}`
@@ -63,6 +81,11 @@ class BucketController {
     }
   }
 
+  /**
+   * 设置 Bucket 访问权限
+   * @method POST
+   * @param {bucket, private}
+   */
   static async bucketPrivate (ctx) {
     let data = {
       bucket: ctx.request.body.bucket || '',
