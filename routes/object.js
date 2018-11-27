6e2b54dcb001bfb6026ac809b2f3d7b24b566ed1
qhx0807 <820240134@qq.com>
2018-11-27 10:30:57 +0800

    update routes

--- routes/object.js
+++ routes/object.js
@@ -2,6 +2,7 @@ const router = require('koa-router')()
 const ObjectCtrl = require('../controllers/objectController')
 
 router.get('metainfo/:bucket/:key', ObjectCtrl.metainfo)
+  .get('deleteAfterDays/:bucket/:key', ObjectCtrl.deleteAfterDays)
 
 
 module.exports = router
